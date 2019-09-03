6def95d7b195c7e3c8afd54c5755d6465f5f8873
Isaias De Leon Reyes <isaias005@hotmail.com>
2019-09-02 20:24:27 -0400

    added fix for server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const serveStatic = require('serve-static');
 var port = process.env.PORT || 5000;
 
 app = express();
-app.use(serveStatic(__dirname + "../client/dist"));
+app.use(serveStatic(__dirname + "/../client/dist"));
 
 app.listen(port);
 console.log('Server started ' + port);
